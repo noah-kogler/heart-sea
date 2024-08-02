6215a51dd87d2ef26bcbe8f740c8fddd8431281e
Noah Kogler <noah.kogler@gmail.com>
2024-08-02 11:50:05 +0200

    improve sitemap to avoid pointing to redirected URLs

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,41 +3,37 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.throwyourheartintothesea.com/',
+      url: 'https://www.throwyourheartintothesea.com/en',
       lastModified: new Date(),
       alternates: {
         languages: {
-          en: 'https://www.throwyourheartintothesea.com/en',
           de: 'https://www.throwyourheartintothesea.com/de',
         },
       },
     },
     {
-      url: 'https://www.throwyourheartintothesea.com/trailer',
+      url: 'https://www.throwyourheartintothesea.com/en/trailer',
       lastModified: new Date(),
       alternates: {
         languages: {
-          en: 'https://www.throwyourheartintothesea.com/en/trailer',
           de: 'https://www.throwyourheartintothesea.com/de/trailer',
         },
       },
     },
     {
-      url: 'https://www.throwyourheartintothesea.com/credits',
+      url: 'https://www.throwyourheartintothesea.com/en/credits',
       lastModified: new Date(),
       alternates: {
         languages: {
-          en: 'https://www.throwyourheartintothesea.com/en/credits',
           de: 'https://www.throwyourheartintothesea.com/de/credits',
         },
       },
     },
     {
-      url: 'https://www.throwyourheartintothesea.com/contact',
+      url: 'https://www.throwyourheartintothesea.com/en/contact',
       lastModified: new Date(),
       alternates: {
         languages: {
-          en: 'https://www.throwyourheartintothesea.com/en/contact',
           de: 'https://www.throwyourheartintothesea.com/de/contact',
         },
       },
